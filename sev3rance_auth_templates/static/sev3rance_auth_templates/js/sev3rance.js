8ff39faf8e8650cd8f44f61e9826a2fb50ddbc15
Peter Pfeufer <info@ppfeufer.de>
2021-09-25 19:21:03 +0200

    duplications with AA 2.9.0 removed

--- sev3rance_auth_templates/static/sev3rance_auth_templates/js/sev3rance.js
+++ sev3rance_auth_templates/static/sev3rance_auth_templates/js/sev3rance.js
@@ -32,57 +32,10 @@ $(document).ready(function () {
     };
 
     /**
-     * check time
-     * @param i
-     * @returns {string}
-     */
-    let checkTime = function (i) {
-        if (i < 10) {
-            i = '0' + i;
-        }
-
-        return i;
-    };
-
-    /**
-     * render a JS clock for Eve Time
-     * @param element
-     * @param utcOffset
-     */
-    let renderClock = function (element, utcOffset) {
-        let today = new Date();
-        let h = today.getUTCHours();
-        let m = today.getUTCMinutes();
-        let s = today.getUTCSeconds();
-
-        h = h + utcOffset;
-
-        if (h > 24) {
-            h = h - 24;
-        }
-
-        if (h < 0) {
-            h = h + 24;
-        }
-
-        h = checkTime(h);
-        m = checkTime(m);
-        s = checkTime(s);
-
-        // document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
-        element.html(h + ':' + m + ':' + s);
-
-        setTimeout(function () {
-            renderClock(element, 0);
-        }, 500);
-    };
-
-    /**
      * functions that need to be executed on load
      */
     let init = function () {
         externalLinks();
-        renderClock($('.eve-time-wrapper .eve-time-clock'), 0);
     };
 
     /**
