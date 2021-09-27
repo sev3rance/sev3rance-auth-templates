ac7977560c821b3bc100161207a86277ccc64950
Peter Pfeufer <info@ppfeufer.de>
2021-09-27 14:51:01 +0200

    test settings fixed

--- testauth/settings.py
+++ testauth/settings.py
@@ -258,10 +258,6 @@ DEBUG = False
 # -7- Auth Templates - https://github.com/sev3rance/sev3rance-auth-templates
 INSTALLED_APPS.insert(0, "sev3rance_auth_templates")
 
-TEMPLATES[0]["OPTIONS"]["context_processors"].append(
-    "sev3rance_auth_templates.context_processors.severance_settings"
-)
-
 # Register an application at https://developers.eveonline.com for Authentication
 # & API Access and fill out these settings. Be sure to set the callback URL
 # to https://example.com/sso/callback substituting your domain for example.com
